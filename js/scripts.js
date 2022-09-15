a77b0a6bdd1dde0d15fe8f1a2aa3eb12289c7111
Ray Consulting <rayconsulting@shopeneur.com>
2022-09-15 12:02:18 -0400

    updated index

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+function updateTimer() {
+    future = Date.parse("oct 15, 2022 9:00:00");
+    now = new Date();
+    diff = future - now;
+
+    if (diff < 0) {
+        document.getElementById("event-start").style.display = 'block';
+        document.getElementById("timer").style.display = 'none';
+    }
+    else {
+        days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        hours = Math.floor(diff / (1000 * 60 * 60));
+        mins = Math.floor(diff / (1000 * 60));
+        secs = Math.floor(diff / 1000);
+
+        d = days;
+        h = hours - days * 24;
+        m = mins - hours * 60;
+        s = secs - mins * 60;
+
+        document.getElementById("timer")
+            .innerHTML =
+            '<div>' + d + '<span>Days</span></div>' +
+            '<div>' + h + '<span>Hours</span></div>' +
+            '<div>' + m + '<span>Minutes</span></div>' +
+            '<div>' + s + '<span>Seconds</span></div>';
+    }
+}
+setInterval('updateTimer()', 1000);
